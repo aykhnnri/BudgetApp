e12951ee6a083782d420a0d2a3380b4ea4ba95f3
aykha <aykha@LAPTOP-2LB44SOS>
2023-11-05 14:18:14 -0600

    Add project files.

---
+++ BudgetApp/Areas/Identity/Data/BudgetAppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BudgetApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the BudgetAppUser class
+public class BudgetAppUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNum { get; set; }
+    public string PostalCode { get; set; }
+
+}
